93f7a5ccf6e8fdc0fd946c1ccde2fbfcff4f4578
anna_gh <anna_ghazaryan@instigatedesign.com>
2024-08-10 00:17:09 +0400

    Author: Anna Description:     - Added application logo.

--- main.cpp
+++ main.cpp
@@ -1,9 +1,13 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
+#include <QIcon>
 
 int main(int argc, char *argv[])
 {
     QGuiApplication app(argc, argv);
+
+    // Set the application icon
+    app.setWindowIcon(QIcon(":/resources/images/logo.jpg"));
 
     QQmlApplicationEngine engine;
     engine.load(QUrl(QStringLiteral("qrc:/qml/Main.qml")));
